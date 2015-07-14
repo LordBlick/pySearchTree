714921a5a48248cffca2be895ccfd0dd5a48bedb
LordBlick <LordBlick@gmail.com>
2015-07-14 23:31:58 +0200

    Initial files upload

---
+++ txtViewSrch.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
+# -*- tabstop: 4 -*-
+
+'''
+ This program source code file is shared library for easy pygtk+2 TextView widget
+searching.
+ 
+ Copyright  © 2015 by LordBlick (at) gmail.com
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, you may find one here:
+ http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ or you may search the http://www.gnu.org website for the version 2 license,
+ or you may write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+'''
+
+import gtk
+Height = 20
+
+def Label(txtLabel, hFixed, posX, posY, width, height=None, fontDesc=None, xalign=None, selectable=False):
+	hLabel = gtk.Label(txtLabel)
+	if fontDesc:
+		hLabel.modify_font(fontDesc)
+	if type(xalign)==float and(0.<=xalign<=1.):
+		yalign = hLabel.get_alignment()[1]
+		hLabel.set_alignment(xalign, yalign)
+	if type(selectable)==bool:
+		hLabel.set_selectable(selectable)
+	hLabel.show()
+	if not height:
+		height=Height
+	hLabel.set_size_request(width, height)
+	if hFixed:
+		hFixed.put(hLabel, posX, posY)
+	return hLabel
+
+def Butt(txtLabel, hFixed, posX, posY, width, height=None, fileImage=None, stockID=None, fontDesc=None):
+	"""If stockID is set, txtLabel set as True means full stock button,
+	non-null string - own Label for stock image,
+	in other case - button with only stock image"""
+	if stockID == None and fileImage == None:
+		hButt = gtk.Button(label=txtLabel, use_underline=False)
+		if fontDesc:
+			hLabel = hButt.child
+			hLabel.modify_font(fontDesc)
+	else:
+		if type(txtLabel)==int or type(txtLabel)==float or type(txtLabel)==type(None) or (type(txtLabel)==str and txtLabel==''):
+			txtLabel = bool(txtLabel)
+		if type(txtLabel)==bool and txtLabel==True or type(txtLabel)==str:
+			if stockID:
+				hButt = gtk.Button(stock=stockID)
+			elif fileImage:
+				image = gtk.Image()
+				image.set_from_file(fileImage)
+				hButt = gtk.Button()
+				hButt.add(image)
+			if type(txtLabel)==str:
+				hLabel = hButt.get_children()[0].get_children()[0].get_children()[1]
+				hLabel.set_text(txtLabel)
+				if fontDesc:
+					hLabel.modify_font(fontDesc)
+		else:
+			image = gtk.Image()
+			if stockID:
+				image.set_from_stock(stockID, gtk.ICON_SIZE_BUTTON)
+			elif fileImage:
+				image.set_from_file(fileImage)
+			hButt = gtk.Button()
+			hButt.add(image)
+	if not height:
+		height = Height
+	hButt.set_size_request(width, height)
+	hFixed.put(hButt, posX, posY)
+	return hButt
+
+def entryIcoClr(ed, icoPos, sigEvent):
+	if icoPos == gtk.ENTRY_ICON_SECONDARY:
+		ed.set_text('')
+
+def Entry(hFixed, posX, posY, width, height=None, startIco=None, clearIco=False, bEditable=True, fontDesc=None):
+	hEntry = gtk.Entry()
+	if fontDesc:
+		hEntry.modify_font(fontDesc)
+	if startIco:
+		textInput.set_icon_from_pixbuf(0, startIco)
+	if clearIco:
+		hEntry.set_icon_from_stock(1, gtk.STOCK_CLOSE)
+		hEntry.set_icon_tooltip_text (1, 'Clear')
+		hEntry.connect("icon-release", entryIcoClr)
+	hEntry.set_property("editable", bool(bEditable))
+	if not height:
+		height = Height
+	hEntry.set_size_request(width, height)
+	hFixed.put(hEntry, posX, posY)
+	return hEntry
+
+class searchTextView:
+	def __init__(it, ui, mainWindow, textView):
+		it.ui = ui
+		it.mainWindow = mainWindow
+		it.textView = textView
+		it.dlgSrch = dlgSrchPos = None
+
+	def dialogFind(it):
+		dlg = it.dlgSrch = gtk.Window(gtk.WINDOW_TOPLEVEL)
+		if hasattr(it.ui, 'accGroup'):
+			dlg.add_accel_group(it.ui.accGroup)
+		dlg.set_border_width(5)
+		dlg.set_resizable(False)
+		dlg.set_title('Find')
+		dlg.set_transient_for(it.mainWindow)
+		dlg.set_destroy_with_parent(True)
+		dlg.set_deletable(False)
+		dlg.set_skip_taskbar_hint(False)
+		# # # # # # # # # # # # # # # # # # # # # # # # #
+		dlgFrame = gtk.Fixed()
+
+		Label("Please enter phrase to find:", dlgFrame, 0, 0, 150)
+		dlg.Entry = Entry(dlgFrame, 0, 25, 200, clearIco=True)
+		dlg.Entry.connect("changed", it.searchFor, 'interactive')
+		bp = dlg.buttonPrev = Butt("Previous", dlgFrame, 0, 50, 70, stockID=gtk.STOCK_GO_BACK)
+		bp.connect("clicked", it.searchFor, 'backward')
+		bn = dlg.buttonNext = Butt("Next", dlgFrame, 75, 50, 70, stockID=gtk.STOCK_GO_FORWARD)
+		bn.connect("clicked", it.searchFor, 'forward')
+		hButtonOK = Butt("OK", dlgFrame, 170, 50, 30)
+		hButtonOK.connect("clicked", lambda xargs: it.hideDlgSrch())
+		dlg.found = None
+		dlg.flags = gtk.TEXT_SEARCH_TEXT_ONLY | gtk.TEXT_SEARCH_VISIBLE_ONLY
+
+		dlg.add(dlgFrame)
+		dlg.show_all()
+
+	def showDlgSrch(it, widget):
+		if it.dlgSrch:
+			it.dlgSrch.present()
+		else:
+			it.dialogFind()
+		dlg = it.dlgSrch
+		if it.dlgSrchPos:
+			dlg.move(*it.dlgSrchPos)
+		txtBuff = it.textView.get_buffer()
+		sel = txtBuff.get_selection_bounds()
+		if sel:
+			dlg.found = sel
+			dlg.Entry.set_text(txtBuff.get_text(*sel))
+		dlg.set_keep_above(True)
+
+	def hideDlgSrch(it):
+		dlg = it.dlgSrch
+		if dlg and(dlg.get_property("visible")):
+			it.dlgSrchPos = dlg.get_position()
+			dlg.hide()
+
+	def getFound(it, txtBuff, srchType, txtSrch):
+		dlg = it.dlgSrch
+		if it.textView.changed:
+			print("skip")
+			dlg.found = None
+			it.textView.changed = False
+		if dlg.found:
+			if srchType in ('interactive', 'backward'):
+				iterB = dlg.found[0]
+			else:
+				iterB = dlg.found[1]
+		else:
+			iterB = None
+		if not(iterB):
+			if srchType in ('interactive', 'forward'):
+				iterB = txtBuff.get_start_iter()
+			else:
+				iterB = txtBuff.get_end_iter()
+		if srchType in ('interactive', 'forward'):
+			return iterB.forward_search(txtSrch, dlg.flags, None)
+		else:
+			return iterB.backward_search(txtSrch, dlg.flags, None)
+
+	def searchFor(it, widget, srchType):
+		dlg = it.dlgSrch
+		txtSrch = dlg.Entry.get_text()
+		if txtSrch:
+			txtBuff = it.textView.get_buffer()
+			lastfound = dlg.found
+			found = dlg.found = it.getFound(txtBuff, srchType, txtSrch)
+			if lastfound and(not(found)):
+				found = dlg.found = it.getFound(txtBuff, srchType, txtSrch)
+			if found:
+				it.textView.scroll_to_iter (found[0], 0)
+				txtBuff.select_range(*found)
---
+++ uiMassFind.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python2
+# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
+'''
+ This program source code file is part of pySearchTree, a text files search application.
+ 
+ Copyright  © 2015 by LordBlick (at) gmail.com
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, you may find one here:
+ http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ or you may search the http://www.gnu.org website for the version 2 license,
+ or you may write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+'''
+
+from txtViewSrch import searchTextView
+from wgts import gtk, pango
+import wgts as wg
+class massFindUI:
+	def __init__(ui):
+		ui.fontDesc = pango.FontDescription('Univers,Sans Condensed 7')
+		ui.fontSmall = pango.FontDescription('Univers,Sans Condensed 8')
+		ui.fontFixedDesc = pango.FontDescription('Terminus,Monospace Bold 7')
+		wg.BGcolor = gtk.gdk.Color('#383430')
+		wg.FGcolor = gtk.gdk.Color('#FFF')
+		wg.BGcolorEntry = gtk.gdk.Color('#201810')
+		wg.Height = 22
+		ui.uiInit()
+		if __name__ == "__main__":
+			ui.mainWindow.connect("destroy", lambda w: gtk.main_quit())
+			ui.buttonExit.connect("clicked", lambda w: gtk.main_quit())
+			ui.logView.insert_end("User Interface Test...\n")
+			ui.uiEnter()
+
+	uiEnter = lambda ui: gtk.main()
+	uiExit = lambda ui: gtk.main_quit()
+
+	def uiInit(ui):
+		from os import chdir as cd, path as ph
+		cd(ph.dirname(ph.abspath(__file__)))
+		if __name__ == "__main__":
+			ui.cfg = {}
+		from gobject import TYPE_STRING as goStr, TYPE_INT as goInt, TYPE_PYOBJECT as goPyObj
+		ui.title="pySearchTree V.0.8"
+		ui.mainWindow = gtk.Window(gtk.WINDOW_TOPLEVEL)
+		w, h = ui.wBase, ui.hBase = (510, 350)
+		ui.mainWindow.set_geometry_hints(
+			min_width=w, min_height=h)
+		ui.mainWindow.resize(w, h)
+		ui.mainWindow.set_title(ui.title)
+		ui.mainWindow.set_border_width(5)
+		ui.accGroup = gtk.AccelGroup()
+		ui.mainWindow.add_accel_group(ui.accGroup)
+		ui.mainWindow.modify_bg(gtk.STATE_NORMAL, wg.BGcolor)
+		ui.cfBPixbuf = gtk.gdk.pixbuf_new_from_file("pic/logview.png")
+		gtk.window_set_default_icon_list(ui.cfBPixbuf, )
+		
+		ui.mainFrame = gtk.Fixed()
+
+		ui.logView = wg.TextView(ui.mainFrame, 5, 5, 0, 0,
+			bEditable=False, tabSpace=4, fontDesc = ui.fontFixedDesc)
+		ui.stv = searchTextView(ui, ui.mainWindow, ui.logView)
+
+		ui.labFileset = wg.Label("Type:", ui.mainFrame, 0, 0, 32)
+		ui.lsFileset = gtk.ListStore(goStr, goStr, goPyObj)
+		ui.cbFileset = wg.ComboBox(ui.lsFileset, ui.mainFrame, 0, 0, 130, wrap=2)
+
+		ui.toggRoot = wg.Toggle("Choose dir...\t", ui.mainFrame, 0,  0, 0)
+
+		ui.toggMaskHome = wg.Toggle("~", ui.mainFrame, 0,  0, 25)
+		ui.toggMaskHome.set_tooltip_text('Toggle Mask Home')
+
+		ui.toggSrchInfo = wg.Toggle("?", ui.mainFrame, 0,  0, 25)
+		ui.toggSrchInfo.set_tooltip_text('Toggle Startup Search Info')
+
+		ui.buttonSearchLog = wg.Butt(None, ui.mainFrame, 0, 0, 25, height=25, stockID=gtk.STOCK_FIND)
+		ui.buttonSearchLog.add_accelerator("clicked", ui.accGroup, ord('F'),
+			gtk.gdk.CONTROL_MASK, gtk.ACCEL_VISIBLE)
+		ui.buttonSearchLog.connect("clicked", ui.stv.showDlgSrch)
+
+		ui.labFindPhrase = wg.Label("Phrase:", ui.mainFrame, 0, 0, 37)
+		ui.txtFindPhrase = wg.Entry(ui.mainFrame, 0, 0, 190, clearIco=True)
+
+		ui.buttonFind = wg.Butt("Find", ui.mainFrame, 0,  0, 50)
+
+		ui.buttonBreak = wg.Butt("Stop", ui.mainFrame, 0,  0, 50)
+
+		ui.buttonClear = wg.Butt("Clear", ui.mainFrame, 0,  0, 50)
+		ui.buttonClear.connect("clicked", lambda x: ui.logView.clear_text())
+
+		ui.buttonExit = wg.Butt("Exit (Ctrl+Q)", ui.mainFrame, 0, 0, 80)
+		ui.buttonExit.add_accelerator(
+			"clicked",
+			ui.accGroup,
+			ord('Q'),
+			gtk.gdk.CONTROL_MASK,
+			gtk.ACCEL_VISIBLE)
+		
+		ui.mainWindow.add(ui.mainFrame)
+		ui.mainWindow.show_all()
+		ui.mainWindow.set_keep_above(True)
+		ui.lastWinSize = None
+		ui.mainWindow.connect("configure-event", ui.uiSize)
+
+	def uiSize(ui, window, event):
+		if event.type==gtk.gdk.CONFIGURE:
+			w, h = event.width, event.height
+			if ui.lastWinSize==(w, h):
+				return True
+			ui.lastWinSize = w, h
+		h1, h2 = h-70, h-40
+		ui.logView.set_size_request(w-20, h-80)
+		ui.mainFrame.move(ui.labFileset, 5, h1)
+		ui.mainFrame.move(ui.cbFileset, 40, h1-2)
+		ui.mainFrame.move(ui.toggRoot, 175, h1)
+		ui.toggRoot.set_size_request(w-280, wg.Height)
+		ui.mainFrame.move(ui.toggMaskHome, w-100, h1)
+		ui.mainFrame.move(ui.toggSrchInfo,  w-70, h1)
+		ui.mainFrame.move(ui.buttonSearchLog, w-40, h-71)
+		ui.mainFrame.move(ui.labFindPhrase, 5, h2)
+		ui.mainFrame.move(ui.txtFindPhrase, 45, h-41)
+		ui.txtFindPhrase.set_size_request(w-320, 25)
+		ui.mainFrame.move(ui.buttonFind, w-265, h2)
+		ui.mainFrame.move(ui.buttonBreak, w-210, h2)
+		ui.mainFrame.move(ui.buttonClear, w-155, h2)
+		ui.mainFrame.move(ui.buttonExit, w-95, h2)
+		return True
+
+	def restoreGeometry(ui):
+		if hasattr(ui, 'stv') and(ui.cfg['dlgSrchPos']):
+			ui.stv.dlgSrchPos =  tuple(map(lambda k: int(k), ui.cfg['dlgSrchPos'].split(',')))
+		ui.rGeo(ui.mainWindow, 'MainWindowGeometry')
+
+	def storeGeometry(ui):
+		if hasattr(ui, 'stv'):
+			stv = ui.stv
+			stv.hideDlgSrch()
+			if stv.dlgSrchPos:
+				ui.cfg['dlgSrchPos'] = "%i,%i" % stv.dlgSrchPos
+		ui.cfg['MainWindowGeometry'] = ui.sGeo(ui.mainWindow)
+
+# Entry point
+if __name__ == "__main__":
+	massFindUI()
